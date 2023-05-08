c940a3353fae6b516421cb0674d6f62d8b973729
Kazuki Ota <kaota@microsoft.com>
2023-05-08 14:10:13 +0900

    二重送信対策追加

---
+++ ChatGptLikeBlazorApp/Utils/ProcessScope.cs
@@ -0,0 +1,37 @@
+﻿namespace ChatGptLikeBlazorApp.Utils;
+
+public class ProcessScope
+{
+    public bool IsRunning { get; private set; }
+    public bool IsNotRunning => !IsRunning;
+
+    public IDisposable Start()
+    {
+        if (IsRunning) { return NullProcess.Instance; }
+
+        IsRunning = true;
+        return new Process(this, state => state.IsRunning = false);
+    }
+}
+
+file class NullProcess : IDisposable
+{
+    public static IDisposable Instance { get; } = new NullProcess();
+    public void Dispose()
+    {
+    }
+}
+
+class Process : IDisposable
+{
+    private readonly ProcessScope _state;
+    private readonly Action<ProcessScope> _dispose;
+
+    public Process(ProcessScope state, Action<ProcessScope> dispose)
+    {
+        _state = state;
+        _dispose = dispose;
+    }
+
+    public void Dispose() => _dispose(_state);
+}
